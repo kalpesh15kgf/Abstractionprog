c3b55c473dd4bc86acec1b02446faffd93475c78
kj-duggu <harshvardhan152k@gmail.com>
2022-02-13 17:12:53 +0530

    1st commit

---
+++ Abstractionprog/src/A.java
@@ -0,0 +1,19 @@
+
+abstract class A {
+	abstract public void a();
+}	
+class B extends A
+{
+	public void a() 
+	{
+		System.out.println("kalpesh");
+    }
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		B kj = new B();
+		kj.a();
+	}
+
+}
